827d638a2d3c9188f1003fc8415502f532613184
JosephPScott <131405303+JosephPScott@users.noreply.github.com>
2025-10-22 23:15:14 +0100

    Update Continuum_corrections.py
    License statement added along with docstrings and additional commenting. Process made more general to easily accommodate analysis at different wavelengths.

--- Continuum_corrections.py
+++ Continuum_corrections.py
@@ -1,15 +1,18 @@
 
 # -*- coding: utf-8 -*-
 """
-Last edited on Monday 17 July 2023
+Last edited on Wednesday 17 September 2025
 
 @author: joseph.p.scott@durham.ac.uk
+
+This code is made avaiable under the CC0 1.0 Universal license
 
 A short script evauating the effect of neglecting continuum contributions when calculating the scattering rates of 2S hydrogen.
 The "full" values are calculated using the module "Main_calculation_functions" in the same project.
 Discrete terms are calculated by numerically integrating to obtain the dipole matrix elements, energies are given by the Rydberg theory.
 The main function "Run_plot" produces a plot comparing the discrete sum calculations to the full value
 The properties of these calculations can be controlled by changing the appropriate values in the main function "Run_plot"
+All other functions support "Run_plot" and are not intended to be run independently
 """
 #Import packages
 import Main_calculation_functions as imp
@@ -17,16 +20,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.special as spec
 from scipy import integrate
+import math
 
 
-import os, psutil
-import time
+#import os, psutil
+#import time
 
 nmax = 300
 k = 0.3
 
 def Overall(wav, number, E_int, V_int, H, T, zb, rhs, lhs_pol): #Produces the total scattering rate and outputs overall ratios
-    tic = time.perf_counter()
+    '''
+    Internal function: Calculates the total scattering rates via both methods and outputs their ratios. Also outputs the limit of this summation process.
+    '''
+ #   tic = time.perf_counter()
     freq = (imp.h*imp.c)/(wav*10**(-9)*imp.Eh)
     pol = abs((imp.Implicit_step(H, T, lhs_pol, rhs, E_int, freq) + imp.Implicit_step(H, T, lhs_pol, rhs, E_int, -1*freq))/3)
     Srates = [sum(get_Raman((i, 0), E_int, freq, H, T, rhs, zb, pol)) for i in range(1, 7) if i != 2] #The largest number 7 should be increased if final states of n=7 or higher are possible
@@ -43,6 +50,9 @@ def Overall(wav, number, E_int, V_int, H, T, zb, rhs, lhs_pol): #Produces the to
 
 #Calculation is the same as in imp
 def get_Raman(final_st, E_int, freq, H, T, rhs, zb, pol):
+    '''
+    Internal function: For calculating a given Raman scattering rate, the same in principle as in 'Main_calculation_functions', but at a sub-state more convenient for the current purpose.
+    '''
     E_fin, V_fin = imp.Schrodinger(final_st, nmax, k)
     freq_scatt = -(E_fin - E_int) + freq
     if freq_scatt  <= 0:
@@ -65,18 +75,32 @@ def get_Raman(final_st, E_int, freq, H, T, rhs, zb, pol):
         return angular*P1*(imp.Eh/imp.hbar), angular*P2*(imp.Eh/imp.hbar) #Outputs a tuple of both rates in units of per second
 
 def Running_Disc(wav, pol, number): #For a given maximum number of discrete states, calculates the total scattering rate at a particular wavelength
+    '''
+    Internal function: for calculating the waveength dependent scattering rate as the sum over a specified number of discrete intermediate states as a cumulative series
+    '''
     E_a = -imp.redm*(1/8)
     freq = (imp.h*imp.c)/(wav*10**(-9)*imp.Eh)
-    discs = Disc_Rate(1, 0, freq, E_a, pol, 1, number)+ Disc_Rate(1, 0, freq, E_a, pol, -1, number)+ Disc_Rate(3, 0, freq, E_a, pol, 1, number)+ Disc_Rate(3, 2, freq, E_a, pol, 1, number)
-    if wav < 514:
-        discs = discs + Disc_Rate(4, 0, freq, E_a, pol, 1, number) + Disc_Rate(4, 2,freq, E_a, pol, 1, number)
-    if wav < 443:
-        discs = discs + Disc_Rate(5, 0, freq, E_a, pol, 1, number) + Disc_Rate(5, 2, freq, E_a, pol, 1, number)
-    if wav < 414:
-        discs = discs + Disc_Rate(6, 0, freq, E_a, pol, 1, number) + Disc_Rate(6, 2, freq, E_a, pol, 1, number)
+    Es = imp.Schrodinger((0, 0), nmax, k)[0]
+    
+    discs = Disc_Rate(1, 0, freq, E_a, pol, 1, number)+ Disc_Rate(1, 0, freq, E_a, pol, -1, number)
+    for i in range(2, 6):
+        if wav < (10**9*imp.c*imp.h)/((Es[i] - Es[1])*imp.Eh):
+            discs = discs + Disc_Rate(i+1, 0, freq, E_a, pol, 1, number) + Disc_Rate(i+1, 2,freq, E_a, pol, 1, number)
+        else:
+            return discs
+ #   discs = Disc_Rate(1, 0, freq, E_a, pol, 1, number)+ Disc_Rate(1, 0, freq, E_a, pol, -1, number)+ Disc_Rate(3, 0, freq, E_a, pol, 1, number)+ Disc_Rate(3, 2, freq, E_a, pol, 1, number)
+ #   if wav < 514:
+ #       discs = discs + Disc_Rate(4, 0, freq, E_a, pol, 1, number) + Disc_Rate(4, 2,freq, E_a, pol, 1, number)
+ #   if wav < 443:
+ #       discs = discs + Disc_Rate(5, 0, freq, E_a, pol, 1, number) + Disc_Rate(5, 2, freq, E_a, pol, 1, number)
+ #   if wav < 414:
+ #       discs = discs + Disc_Rate(6, 0, freq, E_a, pol, 1, number) + Disc_Rate(6, 2, freq, E_a, pol, 1, number)
     return discs
 
 def Disc_Rate(n, l, freq, E_a, pol, sign, number):
+    '''
+    Internal function: calculates one scattering rate as the sum over a speicified number of discrete intermediate states in a cumulative series
+    '''
     E_b = -imp.redm*(1/(2*n**2))
     melms = np.cumsum(np.array([Disc_n(p, freq, n, l, E_a, E_b, sign) for p in range(2, number+1)]))
     freq_sc = -(E_b - E_a) + sign*freq
@@ -87,18 +111,33 @@ def Disc_Rate(n, l, freq, E_a, pol, sign, number):
     return np.array([((freq_sc**3)*(freq**2)*2*A*(imp.fsc**5)*(ms**2))/(3*pol*((imp.mp/imp.me)+1))*(imp.Eh/imp.hbar) for ms in melms]) # Outputs a scattering rate in units of per second
 
 def Limit_Disc(wav, pol, number): #For a given maximum number of discrete states, calculates the total scattering rate at a particular wavelength
+    '''
+    Internal function: for calculating the waveength dependent scattering rate as the sum over a specified number of discrete intermediate states as a total limit
+    '''
     E_a = -imp.redm*(1/8)
     freq = (imp.h*imp.c)/(wav*10**(-9)*imp.Eh)
-    discs = Disc_Lim(1, 0, freq, E_a, pol, 1, number)+ Disc_Lim(1, 0, freq, E_a, pol, -1, number)+ Disc_Lim(3, 0, freq, E_a, pol, 1, number)+ Disc_Lim(3, 2, freq, E_a, pol, 1, number)
-    if wav < 514:
-        discs = discs + Disc_Lim(4, 0, freq, E_a, pol, 1, number) + Disc_Lim(4, 2,freq, E_a, pol, 1, number)
-    if wav < 443:
-        discs = discs + Disc_Lim(5, 0, freq, E_a, pol, 1, number) + Disc_Lim(5, 2, freq, E_a, pol, 1, number)
-    if wav < 414:
-        discs = discs + Disc_Lim(6, 0, freq, E_a, pol, 1, number) + Disc_Lim(6, 2, freq, E_a, pol, 1, number)
+    Es = imp.Schrodinger((0, 0), nmax, k)[0]
+    
+    discs = Disc_Lim(1, 0, freq, E_a, pol, 1, number)+ Disc_Lim(1, 0, freq, E_a, pol, -1, number)
+    for i in range(2, 6):
+        if wav < (10**9*imp.c*imp.h)/((Es[i] - Es[1])*imp.Eh):
+            discs = discs + Disc_Lim(i+1, 0, freq, E_a, pol, 1, number) + Disc_Lim(i+1, 2,freq, E_a, pol, 1, number)
+        else:
+            return discs
+
+ #   discs = Disc_Lim(1, 0, freq, E_a, pol, 1, number)+ Disc_Lim(1, 0, freq, E_a, pol, -1, number)+ Disc_Lim(3, 0, freq, E_a, pol, 1, number)+ Disc_Lim(3, 2, freq, E_a, pol, 1, number)
+ #   if wav < 514:
+ #       discs = discs + Disc_Lim(4, 0, freq, E_a, pol, 1, number) + Disc_Lim(4, 2,freq, E_a, pol, 1, number)
+ #   if wav < 443:
+ #       discs = discs + Disc_Lim(5, 0, freq, E_a, pol, 1, number) + Disc_Lim(5, 2, freq, E_a, pol, 1, number)
+ #   if wav < 414:
+ #       discs = discs + Disc_Lim(6, 0, freq, E_a, pol, 1, number) + Disc_Lim(6, 2, freq, E_a, pol, 1, number)
     return discs
 
 def Disc_Lim(n, l, freq, E_a, pol, sign, number):
+    '''
+    Internal function: calculates one scattering rate as the sum over a speicified number of discrete intermediate states as a total limit
+    '''
     E_b = -imp.redm*(1/(2*n**2))
     melms = sum(np.array([Disc_n(p, freq, n, l, E_a, E_b, sign) for p in range(2, number+1)]))
     freq_sc = -(E_b - E_a) + sign*freq
@@ -109,6 +148,9 @@ def Disc_Lim(n, l, freq, E_a, pol, sign, number):
     return ((freq_sc**3)*(freq**2)*2*A*(imp.fsc**5)*(melms**2))/(3*pol*((imp.mp/imp.me)+1))*(imp.Eh/imp.hbar) # Outputs a scattering rate in units of per second
 
 def Disc_n(nP, freq, n, l, E_a, E_b, sign):
+    '''
+    Internal function: calculates the discrete matrix elements that contribure to the sum
+    '''
     E_n = -imp.redm*(1/(2*nP**2))
     melm1 = integrate.quad(lambda r: wavfunc(r, 2, 0)*wavfunc(r, nP, 1)*r**3, 0, np.inf)[0] # matrix elements are calculated by numerical integration
     melm2 = integrate.quad(lambda r: wavfunc(r, n, l)*wavfunc(r, nP, 1)*r**3, 0, np.inf)[0]
@@ -116,15 +158,24 @@ def Disc_n(nP, freq, n, l, E_a, E_b, sign):
     return melms/(E_n - E_a - sign*freq) + melms/(E_n - E_b + sign*freq) 
 
 def NormL(n, l): # Normalising function for the wavelength
-    return np.sqrt(np.math.factorial(n-l-1)/(2*n*np.math.factorial(n+l)))
+    '''
+    Intenal function: Normalises the analytic wavefunciton
+    '''
+    return np.sqrt(math.factorial(n-l-1)/(2*n*math.factorial(n+l)))
 
 def wavfunc(r, n, l): # Hydrogen bound state wavefunctions
+    '''
+    Internal function: Outputs bound state wavefunctions
+    '''
     return NormL(n, l)*((2*imp.redm/n)**(3/2))*((2*r*imp.redm/n)**l)*np.exp(-r*imp.redm/n)*spec.assoc_laguerre(2*r*imp.redm/n, n-l-1, 2*l + 1)
 
 
 
 #The main function of the module which produces the final plot.
 def Run_plot():
+    '''
+    Function designed to poduce the plot which is the main output of this module. No inputs. The function returns a plot and numerical values for the 4 limiting cases.
+    '''
     wavs = [514.646, 443.212, 414.483, 399.451]
     numbers = (20, 100)
 
@@ -137,9 +188,9 @@ def Run_plot():
     
     Fig1 = plt.figure()
     ax1 = Fig1.add_subplot()
-    markers = ["o", "s", "x", "v"]
+    markers = ["o", "s", "x", "v"] # for longer wavelength strings, the user may have to add to this
     plt.xticks(range(2, 21))
-    for i in range(0, 4):
+    for i in range(0, len(wavs)):
         Terms, lim = Overall(wavs[i], numbers, E2, V2, H, T, zb, rhs, lhs_pol)
         print(lim)
         line1 = ax1.plot([2, 20], [lim, lim], linestyle="dashed")
